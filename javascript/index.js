d4e96823b7781e71fde9948bcafb3d4e4e4a4795
adrian-villamil <adrian.villamil@unillanos.edu.co>
2021-10-05 16:29:02 -0500

    Eliminé las líneas de código donde intercambiaban cómo se mostraban los span dentro de los links a

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,3 @@
 document.getElementsByClassName("contenedor")[0].firstElementChild.addEventListener('click', () => {
     document.getElementsByClassName("contenedor")[0].classList.toggle("contenedor-responsive");
-    Array.from(document.getElementsByClassName("enlace-texto")).forEach((item) => {
-        item.classList.toggle("enlace-texto-responsive");
-    })
 });
