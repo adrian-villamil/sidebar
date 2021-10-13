3b771c19116b711464f3f2220150ab143fe485c9
adrian-villamil <adrian.villamil@unillanos.edu.co>
2021-10-13 17:25:23 -0500

    Remplacé el nombre de la clase del nuevo botón del menú y agregué un toggle a las tres barras del menú

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,6 @@
-document.getElementsByClassName("contenedor")[0].firstElementChild.addEventListener('click', () => {
+document.getElementsByClassName("menu-contenedor")[0].firstElementChild.addEventListener('click', () => {
+    Array.from(document.getElementsByClassName("barras")).forEach((item) => {
+        item.classList.toggle("barras-cruz");
+    })
     document.getElementsByClassName("contenedor")[0].classList.toggle("contenedor-responsive");
 });
