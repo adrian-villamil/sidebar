7ab43a00e58d3dde58826ac9898d0eb1c09b07bd
adrian-villamil <adrian.villamil@unillanos.edu.co>
2021-10-22 16:31:14 -0500

    Agregué un toggle al div icono para alternar la visibilidad de los tooltips de los íconos

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,9 @@
 document.getElementsByClassName("menu-contenedor")[0].firstElementChild.addEventListener('click', () => {
     Array.from(document.getElementsByClassName("barras")).forEach((item) => {
         item.classList.toggle("barras-cruz");
-    })
+    });
     document.getElementsByClassName("contenedor")[0].classList.toggle("contenedor-responsive");
+    Array.from(document.getElementsByClassName("icono")).forEach((item) => {
+        item.classList.toggle("icono-tooltip-hidden");
+    });
 });
