b32cf2aa76b2fe57a6178617401353bf9af6c069
adrian-villamil <adrian.villamil@unillanos.edu.co>
2021-10-30 11:47:05 -0500

    Modifiqué unas líneas de código donde cambiaban sólo los nombres de las clases de los elementos a los que tenía que hacerles animaciones como los span de los enlaces

--- javascript/index.js
+++ javascript/index.js
@@ -2,11 +2,12 @@ document.getElementsByClassName("menu-contenedor")[0].firstElementChild.addEvent
     Array.from(document.getElementsByClassName("barras")).forEach((item) => {
         item.classList.toggle("barras-cruz");
     });
-    document.getElementsByClassName("contenedor")[0].classList.toggle("contenedor-responsive");
-    Array.from(document.getElementsByClassName("icono")).forEach((item) => {
-        item.classList.toggle("icono-tooltip-hidden");
+    Array.from(document.getElementsByClassName("enlace-texto")).forEach((item) => {
+        item.classList.toggle("enlace-texto-open");
     });
-    document.getElementsByClassName("dropdown-content")[0].classList.toggle("drop-cont-responsive");
+    Array.from(document.getElementsByClassName("enlaces")).forEach((item) => {
+        item.classList.toggle("enlaces-tooltip-hidden");
+    });
 });
 
 document.getElementsByClassName("dropdown-button")[0].addEventListener("click", () => {
